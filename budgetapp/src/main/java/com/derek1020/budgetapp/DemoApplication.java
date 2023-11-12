5cf389974885a09e1a844b00cedfc62721488be4
Derek Yeung <derekyeungwn@gmail.com>
2023-11-12 09:43:53 +0800

    abc

---
+++ budgetapp/src/main/java/com/derek1020/budgetapp/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.derek1020.budgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//123
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		//derek
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ budgetapp/src/main/java/com/derek1020/budgetapp/exception/UnprocessableEntityException.java
@@ -0,0 +1,8 @@
+package com.derek1020.budgetapp.exception;
+
+public class UnprocessableEntityException extends RuntimeException{
+	
+	public UnprocessableEntityException(String message) {
+		super(message);
+	}
+}
